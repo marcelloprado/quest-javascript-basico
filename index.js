dc96741cf3ad9f3439e05f090a1a618097dbf497
Marcello prado <marcellopradomuller@gmail.com>
2022-05-24 21:22:12 -0300

    Exercício JavaScript básico

---
+++ index.js
@@ -0,0 +1,27 @@
+//Somar
+function soma(numero1, numero2){
+    return numero1 + numero2
+}
+let resultadoDasoma = soma(2, 3)
+console.log('A soma dos dois números é: ' + resultadoDasoma)
+
+//Diminuir
+function diminui(numero1, numero2){
+    return numero1 - numero2
+}
+let resulatadoDiminuir = diminui(8, 3)
+console.log('A subtração dos dois números é: ' + resulatadoDiminuir)
+
+//Dividir
+function dividir(numero1, numero2){
+    return numero1 / numero2
+}
+let resultadoDivisao = dividir(9, 5)
+console.log('A divisão dos dois números é: ' + resultadoDivisao)
+
+//Multiplicar
+function multiplica(numero1, numero2){
+    return numero1 * numero2
+}
+let resultadoDaMultiplicacao = multiplica(9, 7)
+console.log('A multiplicação dos dois números é: ' + resultadoDaMultiplicacao)
